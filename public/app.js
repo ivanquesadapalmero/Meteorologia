285a5af2342c3457f2204e9891ba5aaa6191d154
Iván Quesada Palmero <ivanquesadapalmero@gmail.com>
2019-02-14 13:06:23 +0100

    commit5

--- public/app.js
+++ public/app.js
@@ -6,8 +6,8 @@
 
 
   let colecciones = {
-    articulos: { nombre: 'string', precio: 'number' },
-    clientes: { nombre: 'string', apellidos: 'string' }
+    previsiones: { nombre: 'string', precio: 'number' },
+    municipios: { nombre: 'string', apellidos: 'string' }
 };
 
 let index = `
@@ -20,7 +20,7 @@ let index = `
          <ul style="padding-left: 50px">
            <li><b>Inicio</b>: Esta página con información.</li>
            <li><b>Artículos</b>: Permite realizar operaciones CRUD sobre los artículos de la BD. </li>
-           <li><b>Clientes</b>: Permite realizar operaciones CRUD sobre los clientes de la BD.</li>
+           <li><b>municipios</b>: Permite realizar operaciones CRUD sobre los municipios de la BD.</li>
          </ul>
      </div>`;
 
@@ -29,8 +29,8 @@ let index = `
 window.addEventListener('load', function () {
 
     let i = document.getElementById('inicio');
-    let a = document.getElementById('articulos');
-    let c = document.getElementById('clientes');
+    let a = document.getElementById('previsiones');
+    let c = document.getElementById('municipios');
 
     i.innerHTML = index;
     i.style.display = 'block';
@@ -41,15 +41,15 @@ window.addEventListener('load', function () {
         c.style.display = 'none';  c.innerHTML = '';       
     });
 
-    document.getElementById('menu-articulos').addEventListener('click', function (e) {
-        verDocumentos('articulos');
+    document.getElementById('menu-previsiones').addEventListener('click', function (e) {
+        verDocumentos('previsiones');
         a.style.display = 'block';
         i.style.display = 'none';
         c.style.display = 'none';  c.innerHTML = '';       
     });
 
-    document.getElementById('menu-clientes').addEventListener('click', function (e) {
-        verDocumentos('clientes');
+    document.getElementById('menu-municipios').addEventListener('click', function (e) {
+        verDocumentos('municipios');
         c.style.display = 'block';
         i.style.display = 'none';  
         a.style.display = 'none';  a.innerHTML = '';
